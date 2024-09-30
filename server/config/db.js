38501120abfa15f1e2dbda57b5a6d433535e85c4
farendivaz <dandyfariz9@gmail.com>
2024-09-30 18:35:33 +0700

    Initial commit

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Fariz2002@",
+  password: "",
   database: "invoice_management",
 });
 
